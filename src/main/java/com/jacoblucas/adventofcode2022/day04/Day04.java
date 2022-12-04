ff53d406eed9e291deeb73ba0651a552dc5a9562
Jacob Lucas <jacobl@indeed.com>
2022-12-03 21:37:25 -0800

    Day 4 part 2

--- src/main/java/com/jacoblucas/adventofcode2022/day04/Day04.java
+++ src/main/java/com/jacoblucas/adventofcode2022/day04/Day04.java
@@ -11,22 +11,28 @@ public class Day04 {
     public static List<Pair<List<Integer>, List<Integer>>> parseInput(final List<String> input) {
         return input.stream()
                 .map(str -> str.split(","))
-                .map(strs -> new Pair<>(
-                        IntStream.range(
-                                Integer.parseInt(strs[0].split("-")[0]),
-                                Integer.parseInt(strs[0].split("-")[1]) + 1)
-                                .boxed()
-                                .toList(),
-                        IntStream.range(
-                                Integer.parseInt(strs[1].split("-")[0]),
-                                Integer.parseInt(strs[1].split("-")[1]) + 1)
-                                .boxed()
-                                .toList()))
+                .map(strs -> {
+                    final String[] s0 = strs[0].split("-");
+                    final String[] s1 = strs[1].split("-");
+                    return new Pair<>(
+                            IntStream.range(Integer.parseInt(s0[0]), Integer.parseInt(s0[1]) + 1)
+                                    .boxed()
+                                    .toList(),
+                            IntStream.range(Integer.parseInt(s1[0]), Integer.parseInt(s1[1]) + 1)
+                                    .boxed()
+                                    .toList());
+                })
                 .toList();
     }
 
     public static boolean isContainedBy(final List<Integer> left, final List<Integer> right) {
         return left.get(0) <= right.get(0) && left.get(left.size()-1) >= right.get(right.size()-1);
+    }
+
+    public static boolean overlaps(final List<Integer> left, final List<Integer> right) {
+        final List<Integer> smaller = left.get(0) < right.get(0) ? left : right;
+        final List<Integer> larger = left.get(0) < right.get(0) ? right : left;
+        return smaller.get(smaller.size()-1) >= larger.get(0);
     }
 
     public static long countFullyContained(final List<Pair<List<Integer>, List<Integer>>> pairs) {
@@ -35,13 +41,22 @@ public class Day04 {
                 .count();
     }
 
+    public static long countOverlaps(final List<Pair<List<Integer>, List<Integer>>> pairs) {
+        return pairs.stream()
+                .filter(p -> Day04.overlaps(p.first(), p.second()))
+                .count();
+    }
+
     public static void main(String[] args) throws IOException {
         final List<String> input = InputReader.read("day04-input.txt");
-
         final List<Pair<List<Integer>, List<Integer>>> assignmentPairs = parseInput(input);
 
         // Part 1
         long fullyContainedAssignmentPairs = countFullyContained(assignmentPairs);
         System.out.println(fullyContainedAssignmentPairs);
+
+        // Part 2
+        long overlaps = countOverlaps(assignmentPairs);
+        System.out.println(overlaps);
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2022/day04/Day04Test.java
+++ src/test/java/com/jacoblucas/adventofcode2022/day04/Day04Test.java
@@ -23,7 +23,9 @@ public class Day04Test {
                 new Pair<>(ImmutableList.of(2,3,4), ImmutableList.of(6,7,8)),
                 new Pair<>(ImmutableList.of(2,3), ImmutableList.of(4,5)),
                 new Pair<>(ImmutableList.of(5,6,7), ImmutableList.of(7,8,9)),
-                new Pair<>(ImmutableList.of(2,3,4,5,6,7,8), ImmutableList.of(3,4,5,6,7))
+                new Pair<>(ImmutableList.of(2,3,4,5,6,7,8), ImmutableList.of(3,4,5,6,7)),
+                new Pair<>(ImmutableList.of(6), ImmutableList.of(4,5,6)),
+                new Pair<>(ImmutableList.of(2,3,4,5,6), ImmutableList.of(4,5,6,7,8))
         )));
     }
 
@@ -41,5 +43,23 @@ public class Day04Test {
     public void testCountFullyContained() {
         long count = Day04.countFullyContained(Day04.parseInput(INPUT));
         assertThat(count, is(2L));
+    }
+
+    @Test
+    public void testOverlaps() {
+        final List<Pair<List<Integer>, List<Integer>>> pairs = Day04.parseInput(INPUT);
+
+        assertThat(Day04.overlaps(pairs.get(0).first(), pairs.get(0).second()), is(false));
+        assertThat(Day04.overlaps(pairs.get(1).first(), pairs.get(1).second()), is(false));
+        assertThat(Day04.overlaps(pairs.get(2).first(), pairs.get(2).second()), is(true));
+        assertThat(Day04.overlaps(pairs.get(3).first(), pairs.get(3).second()), is(true));
+        assertThat(Day04.overlaps(pairs.get(4).first(), pairs.get(4).second()), is(true));
+        assertThat(Day04.overlaps(pairs.get(5).first(), pairs.get(5).second()), is(true));
+    }
+
+    @Test
+    public void testCountOverlaps() {
+        long count = Day04.countOverlaps(Day04.parseInput(INPUT));
+        assertThat(count, is(4L));
     }
 }
