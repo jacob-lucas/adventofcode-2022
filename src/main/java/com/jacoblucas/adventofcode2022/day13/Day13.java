c1ded9f4bd12640479787f267c050c7adcf34d70
Jacob Lucas <jacobl@indeed.com>
2022-12-17 08:57:26 +0800

    Day 13 part 2

--- src/main/java/com/jacoblucas/adventofcode2022/day13/Day13.java
+++ src/main/java/com/jacoblucas/adventofcode2022/day13/Day13.java
@@ -1,13 +1,17 @@
 package com.jacoblucas.adventofcode2022.day13;
 
+import com.google.common.collect.ImmutableList;
 import com.jacoblucas.adventofcode2022.utils.InputReader;
 import com.jacoblucas.adventofcode2022.utils.Pair;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Day13 {
+    public static final Packet D1 = Packet.parse("[[2]]");
+    public static final Packet D2 = Packet.parse("[[6]]");
 
     public static int getOrderedPairIndexSum(final List<Pair<Packet, Packet>> packetPairs) {
         return IntStream.range(0, packetPairs.size())
@@ -23,6 +27,16 @@ public class Day13 {
                 .sum();
     }
 
+    public static List<Packet> sort(final List<Pair<Packet, Packet>> packetPairs) {
+        final ArrayList<Pair<Packet, Packet>> arrayList = new ArrayList<>(packetPairs);
+        arrayList.add(new Pair<>(D1, D2));
+        return arrayList.stream()
+                .map(p -> ImmutableList.of(p.first(), p.second()))
+                .flatMap(List::stream)
+                .sorted(Packet::compareTo)
+                .toList();
+    }
+
     public static void main(String[] args) throws IOException {
         final List<List<String>> input = InputReader.readGroups("day13-input.txt");
 
@@ -36,5 +50,11 @@ public class Day13 {
         // Part 1
         final int sumOfIndices = getOrderedPairIndexSum(packetPairs);
         System.out.println(sumOfIndices);
+
+        // Part 2
+        final List<Packet> sortedPackets = sort(packetPairs);
+        final int index1 = sortedPackets.indexOf(D1);
+        final int index2 = sortedPackets.indexOf(D2);
+        System.out.println((index1+1) * (index2+1));
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2022/day13/Day13Test.java
+++ src/test/java/com/jacoblucas/adventofcode2022/day13/Day13Test.java
@@ -11,6 +11,7 @@ import static com.jacoblucas.adventofcode2022.day13.PacketTest.p10;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p11;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p12;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p13;
+import static com.jacoblucas.adventofcode2022.day13.PacketTest.p14;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p15;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p16;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p2;
@@ -23,20 +24,43 @@ import static com.jacoblucas.adventofcode2022.day13.PacketTest.p8;
 import static com.jacoblucas.adventofcode2022.day13.PacketTest.p9;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInRelativeOrder;
 
 public class Day13Test {
+    private final List<Pair<Packet, Packet>> packetPairs = ImmutableList.of(
+            new Pair<>(p1, p2),
+            new Pair<>(p3, p4),
+            new Pair<>(p5, p6),
+            new Pair<>(p7, p8),
+            new Pair<>(p9, p10),
+            new Pair<>(p11, p12),
+            new Pair<>(p13, p14),
+            new Pair<>(p15, p16));
     @Test
     public void testExampleInput() {
-        final List<Pair<Packet, Packet>> packetPairs = ImmutableList.of(
-                new Pair<>(p1, p2),
-                new Pair<>(p3, p4),
-                new Pair<>(p5, p6),
-                new Pair<>(p7, p8),
-                new Pair<>(p9, p10),
-                new Pair<>(p11, p12),
-                new Pair<>(p13, p13),
-                new Pair<>(p15, p16));
-
         assertThat(Day13.getOrderedPairIndexSum(packetPairs), is(13));
+    }
+
+    @Test
+    public void testSort() {
+        final List<Packet> sorted = Day13.sort(packetPairs);
+        assertThat(sorted, containsInRelativeOrder(
+                p11,
+                p14,
+                p13,
+                p1,
+                p2,
+                p3,
+                p16,
+                p15,
+                p4,
+                Day13.D1,
+                p12,
+                p7,
+                p8,
+                Day13.D2,
+                p6,
+                p5
+        ));
     }
 }
