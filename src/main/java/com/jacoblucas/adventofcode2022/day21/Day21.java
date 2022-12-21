0c157efb8cdbd58fddf0b466d5092bfbc6778e9b
Jacob Lucas <jacobl@indeed.com>
2022-12-21 21:13:47 +0800

    Day 21 part 2

--- src/main/java/com/jacoblucas/adventofcode2022/day21/Day21.java
+++ src/main/java/com/jacoblucas/adventofcode2022/day21/Day21.java
@@ -1,8 +1,10 @@
 package com.jacoblucas.adventofcode2022.day21;
 
+import com.google.common.collect.Lists;
 import com.jacoblucas.adventofcode2022.utils.InputReader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +19,9 @@ public class Day21 {
         long number = resolve("root", input);
         System.out.println(monkeyNumberMap);
         System.out.println(number);
+
+        // Part 2
+        System.out.println(solveFor("humn", input));
     }
 
     public static long resolve(final String name, final List<String> input) {
@@ -24,10 +29,7 @@ public class Day21 {
             return monkeyNumberMap.get(name);
         }
 
-        final String inputLine = input.stream()
-                .filter(str -> str.startsWith(name))
-                .findFirst()
-                .orElse("");
+        final String inputLine = find(name + ":", input);
 
         final String job = inputLine.split(":")[1].trim();
 
@@ -49,5 +51,68 @@ public class Day21 {
                 default -> -1;
             };
         }
+    }
+
+    public static long solveFor(final String solveFor, final List<String> input) {
+        List<String> path = new ArrayList<>();
+        path.add(solveFor);
+
+        final String root = find("root:", input);
+        String monkey = solveFor;
+        String line = "";
+        while (!line.equals(root)) {
+            line = find(" " + monkey, input);
+            final String m = line.split(":")[0];
+            path.add(m);
+            monkey = m;
+        }
+
+        path = Lists.reverse(path);
+
+        System.out.println("Path to " + solveFor + " = " + path);
+
+        String[] equation = line.split(":")[1].trim().split(" ");
+        String left = equation[0];
+        String right = equation[2];
+
+        String next = path.contains(left) ? left : right;
+        String lookup = path.contains(left) ? right : left;
+
+        return solveForHelper(solveFor, input, find(next+":", input), path, resolve(lookup, input));
+    }
+
+    public static long solveForHelper(String solveFor, List<String> input, String line, List<String> path, long value) {
+        String name = line.split(":")[0];
+        if (name.equals(solveFor)) {
+            return value;
+        }
+
+        String[] equation = line.split(":")[1].trim().split(" ");
+        String left = equation[0];
+        String op = equation[1];
+        String right = equation[2];
+
+        String next = path.contains(left) ? left : right;
+        String lookup = path.contains(left) ? right : left;
+
+        long lookupValue = resolve(lookup, input);
+
+        // invert the operation
+        long result = switch (op) {
+            case "+" -> value - lookupValue;
+            case "-" -> path.contains(left) ? value + lookupValue : lookupValue - value;
+            case "*" -> value / lookupValue;
+            case "/" -> path.contains(left) ? value * lookupValue : lookupValue / value;
+            default -> -1;
+        };
+
+        return solveForHelper(solveFor, input, find(next+":", input), path, result);
+    }
+
+    private static String find(String str, List<String> input) {
+        return input.stream()
+                .filter(s -> s.contains(str))
+                .findFirst()
+                .orElse("");
     }
 }
--- src/test/java/com/jacoblucas/adventofcode2022/day21/Day21Test.java
+++ src/test/java/com/jacoblucas/adventofcode2022/day21/Day21Test.java
@@ -3,31 +3,40 @@ package com.jacoblucas.adventofcode2022.day21;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Day21Test {
+    private static final List<String> INPUT = ImmutableList.of(
+            "root: pppw + sjmn",
+            "dbpl: 5",
+            "cczh: sllz + lgvd",
+            "zczc: 2",
+            "ptdq: humn - dvpt",
+            "dvpt: 3",
+            "lfqf: 4",
+            "humn: 5",
+            "ljgn: 2",
+            "sjmn: drzm * dbpl",
+            "sllz: 4",
+            "pppw: cczh / lfqf",
+            "lgvd: ljgn * ptdq",
+            "drzm: hmdt - zczc",
+            "hmdt: 32");
+
     @Test
     public void testResolve() {
-        final List<String> input = ImmutableList.of(
-                "root: pppw + sjmn",
-                "dbpl: 5",
-                "cczh: sllz + lgvd",
-                "zczc: 2",
-                "ptdq: humn - dvpt",
-                "dvpt: 3",
-                "lfqf: 4",
-                "humn: 5",
-                "ljgn: 2",
-                "sjmn: drzm * dbpl",
-                "sllz: 4",
-                "pppw: cczh / lfqf",
-                "lgvd: ljgn * ptdq",
-                "drzm: hmdt - zczc",
-                "hmdt: 32");
+        assertThat(Day21.resolve("root", INPUT), is(152L));
+        assertThat(Day21.resolve("pppw", INPUT), is(2L));
+        assertThat(Day21.resolve("sjmn", INPUT), is(150L));
+    }
 
-        assertThat(Day21.resolve("root", input), is(152L));
+    @Test
+    public void testSolve() {
+        Day21.resolve("root", INPUT);
+        assertThat(Day21.solveFor("humn", new ArrayList<>(INPUT)), is(301L));
     }
 }
